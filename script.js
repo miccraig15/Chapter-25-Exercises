9f4c953247ceda18aa826637eadefe5776412f8d
miccraig15 <miccraig15@gmail.com>
2022-12-22 15:48:42 -0600

    edited text within script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", function(){
     let form = document.querySelector("form");
     form.addEventListener("simulate", function(event) {
-        let testName = document.querySelector("input[name=testName]");
-        let testDate = document.querySelector("input[name=flightDate]");
-        let rocketType = document.querySelector("input[name=rocketType]");
-        let boosterCount = document.querySelector("input[name=boosterCount]");
-        let windRating = document.querySelector("input[name=windRating]");
-        let productionServers = document.querySelector("input[name=productionServers]");
+        let testName = document.getElementByID("testName");
+        let testDate = document.getElementByID("flightDate");
+        let rocketType = document.getElementByID("rocketType");
+        let boosterCount = document.getElementByID("boosterCount");
+        let windRating = document.getElementByID("windRating");
+        let productionServers = document.getElementByID("productionServers");
         if (testName.value === "" || testDate.value === "" || boosterCount.value === "" || windRating.value === "") {
-            window.alert("All fields are required!")
             event.preventDefault();
+            window.alert("All fields are required!")
         }
     });
 });
